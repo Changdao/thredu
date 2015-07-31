0c41c820b9e386f5b7b96635d9ff642e470e0bc0
Changdao <jameslau@263.net>
2015-07-31 14:58:31 +0800

    basic e2e test is done

--- test/e2e/spec.js
+++ test/e2e/spec.js
@@ -1,8 +1,52 @@
 // spec.js
-describe('Protractor Demo App', function() {
-   it('should have a title', function() {
-       browser.get('http://localhost:3000/course#/');
+describe('Thredu Course List', function() {
 
-           expect(browser.getTitle()).toEqual('THREDU course');
-             });
-   });
+    beforeEach(function() {
+        browser.get('http://localhost:3000/course#/');
+    });
+
+    it('should have a title', function() {
+        
+        expect(browser.getTitle()).toEqual('THREDU course');
+
+    });
+
+
+    it('senario can work', function() {
+        element(by.cssContainingText('.panel-button', 'new')).click();
+        
+        expect(element(by.model('course.hours')).isPresent()).toBe(true);
+
+    });
+
+});
+
+// spec.js
+describe('Thredu Course New', function() {
+
+    beforeEach(function() {
+        browser.get('http://localhost:3000/course#/new');
+    });
+
+    it('should have a title', function() {
+        
+        expect(browser.getTitle()).toEqual('THREDU course');
+
+    });
+
+
+    it('can save course', function() {
+        element(by.model('course.name')).sendKeys('Course Name');
+        element(by.model('course.desc')).sendKeys('Course Desc');
+        element(by.model('course.hours')).sendKeys('30');
+        element(by.buttonText('save')).click();
+        expect(element(by.buttonText('Close')).isPresent()).toBe(true);
+        element(by.buttonText('Close')).click();
+        expect(element(by.cssContainingText('.panel-button','new')).isPresent()).toBe(true);
+
+    });
+
+});
+
+
+
